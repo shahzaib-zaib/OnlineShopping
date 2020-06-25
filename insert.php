4112ad80a055ce0406460962eb4941b6a3b1a4ce
Shahzaib <shahzaibux@gmail.com>
2020-06-25 20:42:24 +0500

    Project Initiaied

--- insert.php
+++ insert.php
@@ -19,10 +19,33 @@
         if($countrows != 0){
             while($balti = mysqli_fetch_array($fetch_execute))
             {
-                echo $balti[1];
-                echo $balti[2];
-                echo $balti[3];
-                echo $balti[4];
+                echo "<!-- Card -->
+                <div class="card">
+                
+                  <!-- Card image -->
+                  <div class="view overlay">
+                    <img class="card-img-top" src="https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).jpg"
+                      alt="Card image cap">
+                    <a href="#!">
+                      <div class="mask rgba-white-slight"></div>
+                    </a>
+                  </div>
+                
+                  <!-- Card content -->
+                  <div class="card-body">
+                
+                    <!-- Title -->
+                    <h4 class="card-title">Card title</h4>
+                    <!-- Text -->
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                      content.</p>
+                    <!-- Button -->
+                    <a href="#" class="btn btn-primary">Button</a>
+                
+                  </div>
+                
+                </div>
+                <!-- Card -->"
 
             }
         }
