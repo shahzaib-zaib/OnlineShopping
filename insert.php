06bdb91fb7a71a8672df05f664e9a41611a2e3c9
Shahzaib <shahzaibux@gmail.com>
2020-06-25 20:28:40 +0500

    Project Initiaied

--- insert.php
+++ insert.php
@@ -17,7 +17,14 @@
         $fetch_execute = mysqli_query($con, $fetch_query);
         $countrows = mysqli_num_rows($fetch_execute);
         if($countrows != 0){
-            
+            while($balti = mysqli_fetch_array($fetch_execute))
+            {
+                echo $balti[1];
+                echo $balti[2];
+                echo $balti[3];
+                echo $balti[4];
+
+            }
         }
         else{
             echo "Table has no Record";
