1d98d0888f1e5770f3e1e2349ef4cb2b04396cc7
Shahzaib <shahzaibux@gmail.com>
2020-06-27 19:55:22 +0500

    Updated Project

--- Edit.php
+++ Edit.php
@@ -13,5 +13,27 @@
         $run = mysqli_query($con, $fetch_data);
         $data = mysqli_fetch_array($run);
     ?>
+    <h2>Update Product</h2>
+    <form action="insert.php" method="post">
+        <p>Enter Product Name</p>
+        <input type="text" name="ProName" id="">
+        <br>
+        <select name="ProCategory" id="">
+            <option value="Electtricity">Electricity</option>
+            <option value="mobile">Mobile</option>
+            <option value="clothes">Clothes</option>
+            <option value="crockery">Crockery</option>
+            <option value="other">Other</option>
+        </select>
+        <br>
+        <p>Enter Product Price</p>
+        <input type="number" name="ProPrice" id="">
+        <br>
+        <p>Enter Product Price</p>
+        <textarea name="ProDescription" id="" cols="20" rows="05"></textarea>
+        <br>
+        <input type="submit" value="Add Product" name="Savebtn" id="">
+
+    </form>
 </body>
 </html>
